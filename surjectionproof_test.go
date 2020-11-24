a8d416ff17ee4fbcc3a37946ca23e0f33bf2af0f
altafan <pietro@vulpem.com>
2020-11-24 17:00:46 +0100

    Fixes after merging surjection refactor

--- surjectionproof_test.go
+++ surjectionproof_test.go
@@ -97,44 +97,6 @@ func TestSurjectionProofGenerateAndVerify(t *testing.T) {
 	}
 }
 
-func TestMaybeInvalidProof(t *testing.T) {
-	ctx, _ := ContextCreate(ContextBoth)
-	defer ContextDestroy(ctx)
-
-	outputAsset := h2b("25b251070e29ca19043cf33ccd7324e2ddab03ecc4ae0b5e77c4fc0e5cf6c95a")
-	outputBf := h2b("b919547b0fe215b1cc259f97ae435d6140d6d68ab62b6ceae216af48a75ed8dd")
-
-	outputGenerator, err := GeneratorGenerateBlinded(ctx, outputAsset, outputBf)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	inputGenerators := make([]Generator, 0)
-	inputAssets := [][]byte{
-		h2b("25b251070e29ca19043cf33ccd7324e2ddab03ecc4ae0b5e77c4fc0e5cf6c95a"),
-		h2b("ed167d1b67cf8c72fdc105e7499003a06745e2c42c7d32ed33d3c6dae06a96dd"),
-	}
-	inputAbfs := [][]byte{
-		h2b("11a0828ded4fa0ebcffced49d7e8118ceba3484363486d43ce04fbbb756dfbf9"),
-		h2b("25dde14dd92c0594a3765667ad0ba3263298426e5c5cf38148587a9cd3b2f936"),
-	}
-	for i, v := range inputAssets {
-		gen, err := GeneratorGenerateBlinded(ctx, v, inputAbfs[i])
-		if err != nil {
-			t.Fatal(err)
-		}
-		inputGenerators = append(inputGenerators, *gen)
-	}
-
-	proof, err := SurjectionProofFromString("0200033e8c1bb14b8bb3163102181b7f932515dad5e5ec02e6d32180c04aad77ec8f0ea12653e52cfc8f6d7854c3d671dda156a2dffa750a08f7c4a04b4644559879a46be28a1aa499f8b9068326f3a0bf764b4a8cb67d80f60d0d856118170e5787b4")
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; i < 10; i++ {
-		fmt.Println(SurjectionProofVerify(ctx, proof, inputGenerators, *outputGenerator))
-	}
-}
-
 func TestMaybeValidProof(t *testing.T) {
 	ctx, _ := ContextCreate(ContextBoth)
 	defer ContextDestroy(ctx)
@@ -166,7 +128,7 @@ func TestMaybeValidProof(t *testing.T) {
 	}
 
 	for j := 0; j < 100; j++ {
-		var firstRes, secondRes, thirdRes bool
+		ress := make([]bool, 0, len(proofs))
 		for i, outputAsset := range outputAssets {
 			outputBf := outputBfs[i]
 
@@ -175,30 +137,23 @@ func TestMaybeValidProof(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			inputGenerators := make([]Generator, 0)
+			inputGenerators := make([]*Generator, 0, len(inputAssets))
 			for i, v := range inputAssets {
 				gen, err := GeneratorGenerateBlinded(ctx, v, inputAbfs[i])
 				if err != nil {
 					t.Fatal(err)
 				}
-				inputGenerators = append(inputGenerators, *gen)
+				inputGenerators = append(inputGenerators, gen)
 			}
 
 			proof, err := SurjectionProofFromString(proofs[i])
 			if err != nil {
 				t.Fatal(err)
 			}
-			if i == 0 {
-				firstRes = SurjectionProofVerify(ctx, proof, inputGenerators, *outputGenerator)
-			}
-			if i == 1 {
-				secondRes = SurjectionProofVerify(ctx, proof, inputGenerators, *outputGenerator)
-			}
-			if i == 2 {
-				thirdRes = SurjectionProofVerify(ctx, proof, inputGenerators, *outputGenerator)
-			}
+			res := SurjectionProofVerify(ctx, proof, inputGenerators, outputGenerator)
+			ress = append(ress, res)
 		}
-		fmt.Println(firstRes, secondRes, thirdRes)
+		fmt.Println(ress)
 	}
 }
 
